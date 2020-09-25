0062292a0bdd4ab1fb93c9ddb5354c378eef5751
Seohyun Yoon <sarahseohyun@gmail.com>
2020-09-25 23:26:58 +0900

    function reefactoring

--- whale/popup/popup.js
+++ whale/popup/popup.js
@@ -1,60 +1,25 @@
-function openSnoweInNewTab() {
-    let url = "http://snowe.sookmyung.ac.kr/";
-    whale.tabs.create({ url });
-};
+const SnoweURL = "http://snowe.sookmyung.ac.kr/";
+const SnowayURL = "http://snoway.sookmyung.ac.kr/";
+const GitRepoURL = "https://github.com/seohyun0120/quick-sook";
+const SnoroseURL = "http://www.snorose.com/";
+const SnowboardURL = "https://snowboard.sookmyung.ac.kr";
+const SookmyungCalURL = "http://www.sookmyung.ac.kr/sookmyungkr/978/subview.do";
+const SookmyungHomeURL = "http://www.sookmyung.ac.kr/";
+const SookmyungPortalURL = "http://snoway.sookmyung.ac.kr/";
+const SookmyungPhoneBookURL = "http://www.sookmyung.ac.kr/sookmyungkr/978/subview.do";
+const SookmyungQuickEnrollmentURL = "https://smap.sookmyung.ac.kr:8443/sap/bc/webdynpro/sap/zcmw2403";
 
-function openSnowboardInNewTab() {
-    let url = "https://snowboard.sookmyung.ac.kr";
-    whale.tabs.create({ url });
-}
-
-function openSookmyungInNewTab() {
-    let url = "http://www.sookmyung.ac.kr/";
-    whale.tabs.create({ url });
-}
-
-function openSookmyungPhoneBook() {
-    let url = "http://www.sookmyung.ac.kr/sookmyungkr/978/subview.do";
+function openURLInNewTab(url) {
     whale.tabs.create({ url });
 }
 
-function openSookmyungCalendar() {
-    let url = "http://www.sookmyung.ac.kr/sookmyungkr/1146/subview.do";
-    whale.tabs.create({ url });
-}
-
-function openSnoway() {
-    let url = "http://snoway.sookmyung.ac.kr/";
-    whale.tabs.create({ url });
-}
-
-function openPortal() {
-    let url = "https://portal.sookmyung.ac.kr/";
-    whale.tabs.create({ url });
-}
-
-function openSnorose() {
-    let url = "http://www.snorose.com/";
-    whale.tabs.create({ url });
-}
-
-function openQuickLinkEnrolment() {
-    let url = "https://smap.sookmyung.ac.kr:8443/sap/bc/webdynpro/sap/zcmw2403";
-    whale.tabs.create({ url });
-}
-
-function openGitRepo() {
-    let url = "https://github.com/seohyun0120/quick-sook";
-    whale.tabs.create({ url });
-}
-
-document.getElementById('clickSnowe').addEventListener('click', openSnoweInNewTab);
-document.getElementById('clickSnowboard').addEventListener('click', openSnowboardInNewTab);
-document.getElementById('clickSookmyung').addEventListener('click', openSookmyungInNewTab);
-document.getElementById('clickSMbook').addEventListener('click', openSookmyungPhoneBook);
-document.getElementById('clickSMCalendar').addEventListener('click', openSookmyungCalendar);
-document.getElementById('clickSnoway').addEventListener('click', openSnoway);
-document.getElementById('clickPortal').addEventListener('click', openPortal);
-document.getElementById('clickSnorose').addEventListener('click', openSnorose);
-document.getElementById('clickSMEnrollment').addEventListener('click', openQuickLinkEnrolment);
-document.getElementById('clickEasterEgg').addEventListener('click', openGitRepo);
+document.getElementById('clickSnowe').addEventListener('click', () => { openURLInNewTab(SnoweURL) }, false);
+document.getElementById('clickSnowboard').addEventListener('click', () => { openURLInNewTab(SnowboardURL) }, false);
+document.getElementById('clickSookmyung').addEventListener('click', () => { openURLInNewTab(SookmyungHomeURL) }, false);
+document.getElementById('clickSMbook').addEventListener('click', () => { openURLInNewTab(SookmyungPhoneBookURL) }, false);
+document.getElementById('clickSMCalendar').addEventListener('click', () => { openURLInNewTab(SookmyungCalURL) }, false);
+document.getElementById('clickSnoway').addEventListener('click', () => { openURLInNewTab(SnowayURL) }, false);
+document.getElementById('clickPortal').addEventListener('click', () => { openURLInNewTab(SookmyungPortalURL) }, false);
+document.getElementById('clickSnorose').addEventListener('click', () => { openURLInNewTab(SnoroseURL) }, false);
+document.getElementById('clickSMEnrollment').addEventListener('click', () => { openURLInNewTab(SookmyungQuickEnrollmentURL) }, false);
+document.getElementById('clickEasterEgg').addEventListener('click', () => { openURLInNewTab(GitRepoURL) }, false);
